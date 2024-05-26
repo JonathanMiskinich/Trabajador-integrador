157820e47ddd1f742b5a0eb6f2e294516f341b7b
JonathanMiskinich <jonathanjosueprog@gmail.com>
2024-05-26 18:57:14 -0300

    Update program.cs
    Delete clase entrenador
    agregada funciones AltaEntrenador y BajaEntrenador
    Modificada Clase Deporte

--- program.cs
+++ program.cs
@@ -7,8 +7,67 @@ namespace Trabajo_Integrador
 	{
 		public static void Main(string[] args)
 		{
+			ArrayList deportes = new ArrayList();
+			ArrayList ninios = new ArrayList();
+			
+			Persona entrenador = new Persona("Juan", "Peron", 3232323);
+			
+			Deporte depor = new Deporte("Futbol");
+			Categoria cat = new Categoria("Infantil", entrenador, 20, 20000, "Martes", "14 a 16");
+			deportes.Add(depor);
+			depor.AgregarCategoria(cat);
+			
+			BajaEntrenador(deportes);
 			
 			Console.ReadKey(true);
+		}
+		public static Persona AltaEntrenador()
+		{
+			Persona entrenadorNuevo = new Persona(); // Se crea una instancia de persona
+			string nombre, apellido;
+			int dni;
+			
+			//Se le piden los datos al usuario
+			Console.WriteLine("Ingrese los datos del entrenador: ");
+			Console.Write("Nombre: ");
+			nombre = Console.ReadLine();
+			
+			Console.Write("Apellido: ");
+			apellido = Console.ReadLine();
+			
+			Console.Write("DNI: ");
+			dni = int.Parse(Console.ReadLine());
+			
+			//Se le asignan los valores a la la instacia de persona antes creada
+			entrenadorNuevo.NOMBRE = nombre;
+			entrenadorNuevo.APELLIDO = apellido;
+			entrenadorNuevo.DNI = dni;
+			
+			return entrenadorNuevo;
+		}
+		
+		public static void BajaEntrenador(ArrayList deporte)
+		{
+			Console.Write("Ingrese el nombre del deporte en el que queire dar de baja al entrenador: ");
+			string nombre = Console.ReadLine();
+			
+			foreach (Deporte elem in deporte) // Recorre el aaray pasado por parametro
+			{
+				if (elem.NOMBRE == nombre) //Se ve si el nombre ingresado es de algun deporte guardado
+				{ 
+					foreach (Categoria elem2 in elem.CATEGORIAS) //Recorre el Array de categorias dentro del objeto deporte
+					{
+						Console.WriteLine("¿La categoria a modificar es {0}?", elem2.DESCRIPCION);
+						string opc = Console.ReadLine();
+						if (opc == "no") {break;}
+						if (opc == "si") 
+						{
+							Console.WriteLine("El entrenador {0} {1} fue dado de baja.", elem2.ENTRENADOR.NOMBRE, elem2.ENTRENADOR.APELLIDO);
+							elem2.ENTRENADOR = null; // Se le asigna un valor nulo al atributo entrenador 
+						}
+					}
+				}
+			}
 		}
 	}
 	
@@ -43,25 +102,6 @@ namespace Trabajo_Integrador
 		}
 	}
 	
-	
-	public class Entrenador: Persona
-	{
-		//Atributos
-		private Categoria categoria;
-		
-		//Constructores
-		public Entrenador(){}
-		public Entrenador (string nombre, string apellido, int dni, Categoria categoria): base (nombre, apellido, dni)
-		{
-			this.categoria = categoria;
-		}
-		//propiedades
-		public Categoria CATEGORIA{
-			get{return this.categoria;}
-			set{this.categoria = value;}
-		}
-		
-	}
 	public class Ninios : Persona
 	{
 	    // Atributos
@@ -121,20 +161,19 @@ namespace Trabajo_Integrador
 	        this.categoriaDeporte = nuevaCategoria;
 	    }
 	    
-	    //Metodo obtener nombre del deporte
 	}
 	
 	public class Categoria
 	{
 		//Atributos
 		private string descripcion, dia, hora;
-		private Entrenador entrenador;
+		private Persona entrenador;
 		private int cupo, cantidadInscriptos;
 		private double costoCuota;
 		
 		//Constructores
 		public Categoria(){}
-		public Categoria(string desc, Entrenador entre, int cupo, double costoCuota, string dia, string hora){
+		public Categoria(string desc, Persona entre, int cupo, double costoCuota, string dia, string hora){
 			this.descripcion = desc;
 			this.entrenador = entre;
 			this.cupo = cupo;
@@ -162,7 +201,7 @@ namespace Trabajo_Integrador
 			set{this.hora = value;}
 		}
 		
-		public Entrenador ENTRENADOR
+		public Persona ENTRENADOR
 		{
 			get{return this.entrenador;}
 			set{this.entrenador = value;}
@@ -190,14 +229,16 @@ namespace Trabajo_Integrador
 	public class Deporte
 	{
 		//atributos
-		protected string nombre;
-		protected ArrayList categorias;
+		private string nombre;
+		private ArrayList categorias;
+		
 		//constructor
 		public Deporte (string nombre)
 		{
 			this.nombre = nombre;
 			this.categorias = new ArrayList();
 		}
+		
 		//propiedades
 		public string NOMBRE{
 			get{return this.nombre;}
@@ -207,18 +248,48 @@ namespace Trabajo_Integrador
 			get{return this.categorias;}
 			set{this.categorias = value;} 
 		}
+		
 		//metodos
 		public void AgregarCategoria(Categoria nuevaCat)
 		{
 			categorias.Add(nuevaCat);
 		}
+		
 		public void EliminarCategoria(Categoria removerCat)
 		{
 			categorias.Remove(removerCat);
 		}
-		public void ModificarCategoria(Categoria catAModificar,Categoria modificarCat)
+		
+		public void verTodasCategorias()
 		{
-			//categorias[catAModificar] = modificarCat;
+			foreach (Categoria cat in categorias)
+			{
+				Console.WriteLine(cat);
+			}
+		}
+		
+		public void DevolverCatNum (int i)
+		{
+			try {
+				Console.WriteLine(categorias[i]);
+			} 
+			catch (IndexOutOfRangeException){
+				Console.WriteLine("El numero ingresado es incorrecto.");
+			}
+		}
+		
+		public bool ExisteCategoria(Categoria categoria)
+		{
+			bool existe = false;
+			if (categorias.Contains(categoria)) {
+				existe = true;
+			}
+			return existe;
+		}
+		
+		public int cantCategorias()
+		{
+			return categorias.Count;
 		}
 	}
 	
