855bb1d6045ac127fba70903253bb1fc564d2a3c
JonathanMiskinich <jonathanjosueprog@gmail.com>
2024-09-09 18:41:32 -0300

    Reorganizacion de carpetas y archivos

---
+++ Trabajo_Integrador/Clases/Persona.cs
@@ -0,0 +1,37 @@
+namespace Clases.Persona
+{
+    public class Persona
+	{
+		//Atributos
+		protected string nombre, apellido;
+		protected int dni;
+		
+		//Constructores
+		public Persona(){}
+		public Persona(string nombre, string apellido, int dni)
+        {
+			this.nombre = nombre;
+			this.apellido = apellido;
+			this.dni = dni;
+		}
+		
+		//Propiedades
+		public string NOMBRE
+        {
+			get{return this.nombre;}
+			set{this.nombre = value;}
+		}
+		
+		public string APELLIDO
+        {
+			get{return this.apellido;}
+			set{this.apellido = value;}
+		}
+		
+		public int DNI
+        {
+			get{return this.dni;}
+			set{this.dni = value;}
+		}
+	}
+}
